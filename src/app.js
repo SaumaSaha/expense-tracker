6663765ae2e0808c4c83177412041437c34afd2b
Sauma Saha <SaumaSaha@users.noreply.github.com>
2023-08-26 17:18:56 +0530

    Create an app listening on port 8000

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const { logRequest } = require("./middlewares/request-logger");
 
 const handleHomePage = (_, res) => res.send("Home Page");
 
 const createApp = () => {
   const app = express();
 
+  app.use(logRequest);
   app.get("/", handleHomePage);
   return app;
 };
