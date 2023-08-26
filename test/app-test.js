adaf4aafbead9df6e8facd4443d1e3ec5ba8de44
Sauma Saha <SaumaSaha@users.noreply.github.com>
2023-08-27 00:24:25 +0530

    Add feature to serve add expense page from server

--- test/app-test.js
+++ test/app-test.js
@@ -1,8 +1,6 @@
-const fs = require("fs");
 const { describe, it, beforeEach } = require("node:test");
 const request = require("supertest");
 const { createApp } = require("../src/app");
-const indexHtml = fs.readFileSync("./public/index.html", "utf-8");
 
 let app = null;
 
@@ -12,7 +10,24 @@ describe("/", () => {
       app = createApp();
     });
     it("should give the home page content", (_, done) => {
-      request(app).get("/").expect(200).expect(indexHtml).end(done);
+      request(app)
+        .get("/")
+        .expect(200)
+        .expect("content-type", /text\/html/)
+        .end(done);
+    });
+  });
+
+  describe("GET /public/add-expense.html", () => {
+    beforeEach(() => {
+      app = createApp();
+    });
+    it("should give the add expense page content", (_, done) => {
+      request(app)
+        .get("/pages/add-expense.html")
+        .expect(200)
+        .expect("content-type", /text\/html/)
+        .end(done);
     });
   });
 });
