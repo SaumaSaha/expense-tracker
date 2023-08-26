92ae3c8271acdcf96746594d809df4b9c467578e
Sauma Saha <SaumaSaha@users.noreply.github.com>
2023-08-26 21:26:41 +0530

    Change the home page and it's style

--- src/app.js
+++ src/app.js
@@ -1,13 +1,10 @@
 const express = require("express");
 const { logRequest } = require("./middlewares/request-logger");
 
-const handleHomePage = (_, res) => res.redirect(303, "/index.html");
-
 const createApp = () => {
   const app = express();
 
   app.use(logRequest);
-  app.get("/", handleHomePage);
   app.use(express.static("public"));
   return app;
 };
--- test/app-test.js
+++ test/app-test.js
@@ -1,6 +1,8 @@
+const fs = require("fs");
 const { describe, it, beforeEach } = require("node:test");
 const request = require("supertest");
 const { createApp } = require("../src/app");
+const indexHtml = fs.readFileSync("./public/index.html", "utf-8");
 
 let app = null;
 
@@ -10,7 +12,7 @@ describe("/", () => {
       app = createApp();
     });
     it("should give the home page content", (_, done) => {
-      request(app).get("/").expect(303).expect("location", "/index.html").end(done);
+      request(app).get("/").expect(200).expect(indexHtml).end(done);
     });
   });
 });
